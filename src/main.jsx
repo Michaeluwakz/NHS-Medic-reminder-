995f991a16cb7c2e0c65ce360cf32c89e37534ab
Michaeluwakz <celon2000@icloud.com>
2025-02-06 00:11:37 +0100

    Changes made

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // Update the import path for index.css
-import "../index.css";
+import "./index.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
